5bafecd9d42927ade820c1ee34b5ac45412b0106
Denys Mykhailov <necuknpocto@gmail.com>
2024-04-21 08:50:34 +0300

    Update animation MST

--- DM-project/myGraph.py
+++ DM-project/myGraph.py
@@ -31,6 +31,8 @@ class Graph:
             node1 = self.nodes_dict[name1]
             node2 = self.nodes_dict[name2]
             node1.add_neighbor(node2, weight)
+        else:
+            print("Error! At least one of the nodes does not exist!")
 
     def copy_without_edges(self) -> 'Graph':
         """Create a copy of the graph without any edges (only nodes)."""
@@ -40,7 +42,7 @@ class Graph:
             copied_graph.add_node(node_name)
         return copied_graph
 
-    def plot_graph(self, title: str = 'Graph Visualization', v_color: str = "#E4E0DC", e_color: str = "#5271FF") -> None:
+    def plot_graph(self, pos=None, title: str = 'Graph Visualization', v_color: str = "#E4E0DC", e_color: str = "#5271FF") -> dict:
         """Plot the graph using networkx and matplotlib."""
         G = nx.Graph()
 
@@ -52,9 +54,9 @@ class Graph:
             for neighbor, weight in node.neighbors:
                 G.add_edge(node.name, neighbor.name, weight=weight)
                 edge_labels[(node.name, neighbor.name)] = f'{weight:.2f}'  # format weight for display
-
-        # Use a fixed layout for consistent node positions
-        pos = nx.circular_layout(G)  # can also try other layouts like nx.shell_layout(G)
+        if not pos:
+            # Use a fixed layout for consistent node positions
+            pos = nx.circular_layout(G)  # can also try other layouts like nx.shell_layout(G)
 
         # Draw the graph with fixed node positions
         nx.draw(G, pos, with_labels=True, node_color=v_color, node_size=400, font_size=12,
@@ -62,6 +64,7 @@ class Graph:
         # draw edge labels
         nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', font_size=10)
         plt.title(title)
+        return pos
 
 
 # usage
@@ -77,16 +80,19 @@ def main():
     g.add_edge('C', 'A', 3.2)
 
     # copy  without edges
-    copied_g = g.copy_without_edges()
-    copied_g.add_edge('B', 'C', 5.8)  # test
-
+    # copied_g = g.copy_without_edges()
+    copied_g = Graph()
+    copied_g.add_node('B')
+    copied_g.add_node('C')
+    copied_g.add_edge('B', 'C', 1.8)  # test
 
     # original graph
-    g.plot_graph(title='Original Graph')
+    original_pos = g.plot_graph(title='Original Graph')
+    print('test' + str(original_pos))
     plt.pause(2)
 
     # graph without edges using the same node positions
-    copied_g.plot_graph(title='Copied Graph (No Edges)', e_color="#7DF848")
+    copied_g.plot_graph(original_pos, title='Copied Graph (No Edges)', v_color="#FF3131", e_color="#7DF848")
 
     plt.show()
 
